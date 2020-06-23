e4fbf13208674bd1182eb64ffe00078c30d1033c
andrea-isoenergy <67228633+andrea-isoenergy@users.noreply.github.com>
2020-06-23 09:14:27 +0100

    Add files via upload

---
+++ Template_R0.py
@@ -0,0 +1,14 @@
+#############################################################################
+# Filename    : Template.py
+# Description :
+# Author      :
+# Date        :
+# Revision    :
+# note        :
+########################################################################
+# Importo le librerie che mi interessano
+
+# Definisco le funzioni che mi servono
+
+
+# Main proram
