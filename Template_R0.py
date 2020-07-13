ee0631bcbf0bd1132300669889e6e269c1b6663c
andrea-isoenergy <andrea.grosso@iso.co.uk>
2020-07-13 22:37:00 +0100

    Nuova version
    Rilasciata nuova versione del template

--- Template_R0.py
+++ Template_R0.py
@@ -1,12 +1,14 @@
 #############################################################################
 # Filename    : Template.py
-# Description :
-# Author      :
-# Date        :
-# Revision    :
-# note        :
+# Description : File di template da usare tutte le volte che scrivo un programma
+# Author      : Andrea
+# Date        : 13-07-2020
+# Revision    : R1
+# note        : Aggiunto la parte relativa alle classi
 ########################################################################
 # Importo le librerie che mi interessano
+
+# Definisco le classi che mi servono
 
 # Definisco le funzioni che mi servono
 
