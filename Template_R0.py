681a5c488e50dd9ede7b96391847fa9e4d85b3b5
andrea-isoenergy <andrea.grosso@iso.co.uk>
2020-09-27 15:31:21 +0100

    Costanti
    Nuova revisione con l'aggiunta delle costanti

--- Template_R0.py
+++ Template_R0.py
@@ -2,11 +2,13 @@
 # Filename    : Template.py
 # Description : File di template da usare tutte le volte che scrivo un programma
 # Author      : Andrea
-# Date        : 13-07-2020
-# Revision    : R1
-# note        : Aggiunto la parte relativa alle classi
+# Date        : 27-09-2020
+# Revision    : R2
+# note        : Aggiunto la parte relativa alle costanti
 ########################################################################
 # Importo le librerie che mi interessano
+
+# Definisco le costanti che mi servono
 
 # Definisco le classi che mi servono
 
